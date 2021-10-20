489ef2e31eed4f3ee44d985a45f70d26c318a24d
Thanuja94 <thanujag94@gmail.com>
2021-10-20 12:51:45 +0530

    change content when navigate

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import * as React from 'react';
-import Container from '@mui/material/Container';
-import Box from '@mui/material/Box';
 import { Grid, Typography } from '@mui/material';
 import ContactForm from './components/contact-form/ContactForm';
-import Stepper from './components/stepper/Stepper';
-import { grid } from '@mui/system';
-import InvestmentPlanForm from './components/investment-plan-form/InvestmentPlanForm';
-import InvestmentPreferencesForm from './components/investment-preferences-form/InvestmentPreferencesForm';
 
 
 
@@ -18,23 +12,9 @@ function App() {
 
     <main>
       <Grid container spacing={2} style={{height:'100vh'}}>
-        <Grid item xs={4} sm={4} md={4} lg={4} style={{backgroundColor:'#35A0EE'}}>
-        <Typography mt= {2} ml={10} sx={{ fontFamily: 'Monospace' }} variant="h5" align="left"  style={{color:'white'}} >
-              UNITEDPROPERTIES
-            </Typography>
-            <div>
-            <Stepper/>
-            </div>
-          
-        </Grid>
-        <Grid item xs={8} sm={8} md={8} lg={8} style={{backgroundColor:'white'}}>
-          {/* <InvestmentPlanForm/> */}
-          <ContactForm/>
-         {/* <InvestmentPreferencesForm/> */}
-         
-        </Grid>
+            <ContactForm/>
       </Grid>
-      
+
     </main>
 
 
--- src/components/contact-form/ContactForm.js
+++ src/components/contact-form/ContactForm.js
@@ -1,64 +1,158 @@
-
-import React, { useEffect, useState } from "react";
-import { Typography } from '@mui/material'
-import { TextField } from '@mui/material'
-import { Box } from '@mui/system'
+import React, {useEffect, useState} from "react";
+import {Grid, Typography} from '@mui/material'
+import {TextField} from '@mui/material'
+import {Box} from '@mui/system'
 import Button from '@mui/material/Button';
 import ArrowForward from '@mui/icons-material/ArrowForward';
 import ArrowBack from '@mui/icons-material/ArrowBack';
-import ContactFormContent from './ContactFormContent';
-import InvestmentPlanContent from '../investment-plan-form/InvestmentPlanContent';
-import InvestmentPreferContent from '../investment-preferences-form/InvestmentPreferContent';
-
-
+import Stepper from '@mui/material/Stepper';
+import Step from "@mui/material/Step";
+import StepLabel from "@mui/material/StepLabel";
+import ContactFormContent from "./ContactFormContent";
+import InvestmentPlanContent from "../investment-plan-form/InvestmentPlanContent";
+import InvestmentPreferContent from "../investment-preferences-form/InvestmentPreferContent";
 
 
 const ContactForm = () => {
-  
-  // const onClickNextStep = () => this.props.history.push("/InvestmentPlanForm");
 
-  return (
-    <body>
-      <Box mt={2} ml={10} mr={10} sx={{ height: 50 }} >
-        <div style={{ display: "flex" }}>
-          <Typography align="left" style={{ color: 'grey' }} >
-            step 1 from 3
+    const steps = [
+        {
+            label: 'Contact Details',
+            description: 'We care about your time, that\'s why we created a 3-stage onboarding that will not a take more than 5 miniuts to complete.',
+            component: <ContactFormContent/>
 
-          </Typography>
-          <Typography pl={30} style={{ color: 'gray' }}>
-            Lost or have trouble? <Button sx={{ width: 110 }} variant="text" style={{ textTransform: "none", color: '#35A0EE' }} endIcon={<ArrowForward />} >
-              Get help
-            </Button>
-          </Typography>
-        </div>
+        },
+        {
+            label: 'Investment plans',
+            description: 'Save from thousands to millions on your deal. Secure the best possible. And get independent, unbiased advise for free.',
+            component: <InvestmentPlanContent/>
+        },
+        {
+            label: 'Investment preferences',
+            description: 'United properties is about fast & easy searching, buying, selling and investing ever - online, with an expert by our side',
+            component: <InvestmentPreferContent/>
 
+        },
+    ];
 
-
-
-      </Box>
-        <Box>
-        {/* <ContactFormContent/> */}
-        {/* <InvestmentPlanContent/> */}
-        <InvestmentPreferContent/>
-        </Box>
-      
+    const [activeStep, setActiveStep] = React.useState(0);
 
 
+    const handleNext = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    };
+
+    const handleBack = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep - 1);
+    };
+
+    const handleReset = () => {
+        setActiveStep(0);
+
+    };
+
+    // const onClickNextStep = () => this.props.history.push("/InvestmentPlanForm");
+
+    return (
+
+        <>
+            <Grid item xs={4} sm={4} md={4} lg={4} style={{backgroundColor: '#35A0EE'}}>
+                <Typography mt={2} ml={10} sx={{fontFamily: 'Monospace'}} variant="h5" align="left"
+                            style={{color: 'white'}}>
+                    UNITEDPROPERTIES
+                </Typography>
+                <Box mt={20} ml={10} mr={10} sx={{maxWidth: 400}}>
+                    <Stepper activeStep={activeStep} orientation="vertical">
+                        {steps.map((step, index) => (
+                            <Step key={step.label}>
+                                <StepLabel
+                                    optional={
+                                        index === 2 ? (
+                                            <Typography variant="caption"> </Typography>
+                                        ) : null
+                                    }
+                                >
+                                    {step.label}
+                                </StepLabel>
+                            </Step>
+                        ))}
+                    </Stepper>
+                </Box>
+                <Box mt={15} ml={10} mr={10} sx={{height: 175}} style={{backgroundColor: 'white', borderRadius: 4}}>
+                    <Typography pl={2} pt={2} sx={{ fontStyle: 'italic', textAlign: 'left' }} style={{color: '#A4AEB4'}}>
+                        {steps[activeStep].description}
+                    </Typography>
+                </Box>
+
+            </Grid>
+            <Grid item xs={8} sm={8} md={8} lg={8} style={{backgroundColor: 'white'}}>
+
+                <Box mt={2} ml={10} mr={10} sx={{height: 50}}>
+                    <div style={{display: "flex"}}>
+                        <Typography align="left" style={{color: 'grey'}}>
+                            step {activeStep + 1} from {steps.length}
+
+                        </Typography>
+                        <Typography pl={30} style={{color: 'gray'}}>
+                            Lost or have trouble? <Button sx={{width: 110}} variant="text"
+                                                          style={{textTransform: "none", color: '#35A0EE'}}
+                                                          endIcon={<ArrowForward/>}>
+                            Get help
+                        </Button>
+                        </Typography>
+                    </div>
 
 
-      <Box mt={2} ml={10} mr={10} sx={{ height: 110 }} style={{ backgroundColor: 'white' }}>
-        <Button sx={{ mt: 5, ml: 2 }} variant="text" size="small" style={{ textTransform: "none", color: '#35A0EE' }} startIcon={<ArrowBack />}>
-          Back to the homepage
-        </Button>
-        <Button sx={{ mt: 5, ml: 15, mr: 2 }} variant="contained" size="medium" style={{ textTransform: "none", backgroundColor: '#CFD8DC', color: '#35A0EE' }} >
-          Skip for now
-        </Button>
-        <Button sx={{ mt: 5, ml: 2 }} variant="contained" size="medium" style={{ textTransform: "none", backgroundColor: '#35A0EE' }} endIcon={<ArrowForward />} >
-          Next step
-        </Button>
-      </Box>
-    </body>
-  );
+                </Box>
+                <Box>
+                    {steps[activeStep].component}
+                </Box>
+
+                <Box mt={2} ml={10} mr={10} sx={{height: 110}} style={{backgroundColor: 'white'}}>
+                    {activeStep == 0 ? (
+                        <Button sx={{mt: 5, ml: 2}} variant="text" size="small"
+                                style={{textTransform: "none", color: '#35A0EE'}} startIcon={<ArrowBack/>}>
+                            Back to the homepage
+                        </Button>
+                    ) : (
+                        <Button sx={{mt: 5, ml: 2}} variant="text" size="small"
+                                onClick={handleBack}
+                                style={{textTransform: "none", color: '#35A0EE'}} startIcon={<ArrowBack/>}>
+                            Back to the previous
+                        </Button>
+                    )}
+                    {activeStep != steps.length - 1 ? (
+                        <>
+                            <Button sx={{mt: 5, ml: 15, mr: 2}} variant="contained" size="medium"
+                                    onClick={handleNext}
+                                    style={{textTransform: "none", backgroundColor: '#CFD8DC', color: '#35A0EE'}}>
+                                Skip for now
+                            </Button>
+                            <Button sx={{mt: 5, ml: 2}} variant="contained" size="medium"
+                                    onClick={handleNext}
+                                    style={{textTransform: "none", backgroundColor: '#35A0EE'}}
+                                    endIcon={<ArrowForward/>}>
+                                Next step
+                            </Button>
+                        </>
+                    ) : (
+                        <>
+                            <Button sx={{mt: 5, ml: 15, mr: 2}} variant="contained" size="medium"
+                                    style={{textTransform: "none", backgroundColor: '#CFD8DC', color: '#35A0EE'}}>
+                                Skip for now
+                            </Button>
+                            <Button sx={{mt: 5, ml: 2}} variant="contained" size="medium"
+                                    style={{textTransform: "none", backgroundColor: '#35A0EE'}}>
+                               Finish
+                            </Button>
+                        </>
+                    )}
+                </Box>
+
+
+            </Grid>
+        </>
+    );
 }
 
 export default ContactForm
